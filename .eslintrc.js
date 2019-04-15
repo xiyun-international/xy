1603045667e2d751f2b769ec389736cd5299486c
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2019-04-15 18:48:04 +0800

    feat: 新增test、library库

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,47 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    browser: true,
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "env": {
+    "browser": true,
+    "jest": true
   },
-  extends: ['plugin:vue/essential', 'prettier'],
-  parserOptions: {
-    parser: 'babel-eslint',
+  "rules": {
+    "generator-star-spacing": [0],
+    "consistent-return": [0],
+    "global-require": [1],
+    "import/prefer-default-export": [0],
+    "no-else-return": [0],
+    "no-restricted-syntax": [0],
+    "import/no-extraneous-dependencies": [0],
+    "no-use-before-define": [0],
+    "jsx-a11y/no-static-element-interactions": [0],
+    "no-nested-ternary": [0],
+    "arrow-body-style": [0],
+    "import/extensions": [0],
+    "no-bitwise": [0],
+    "no-cond-assign": [0],
+    "import/no-unresolved": [0],
+    "require-yield": [1],
+    "no-param-reassign": [0],
+    "no-shadow": [0],
+    "no-underscore-dangle": [0],
+    "spaced-comment": [0],
+    "indent": [0],
+    "quotes": [0],
+    "func-names": [0],
+    "arrow-parens": [0],
+    "space-before-function-paren": [0],
+    "no-useless-escape": [0],
+    "object-curly-newline": [0],
+    "function-paren-newline": [0],
+    "class-methods-use-this": [0],
+    "no-new": [0],
+    "import/newline-after-import": [0],
+    "no-console": [0]
   },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true
+    }
+  }
 };
