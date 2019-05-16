19dfd5b2049187e73ee084fd5ba42b4f8ecab528
yuyue <yuyue@xiyun.com.cn>
2019-05-16 09:41:42 +0800

    feat: 工具集 filters加 邮箱隐藏中间位

--- docs/utils/xy-filters.md
+++ docs/utils/xy-filters.md
@@ -53,3 +53,27 @@ export default {
 }
 </script>
 ```
+#### 隐藏邮箱前两位后的四位：
+
+##### 示例：
+<div>12****3434@163.com</div>
+
+##### 代码实现：
+```vue
+...
+<div>{{mail | hideMailMiddle}}</div>
+...  
+
+<script>
+import { hideMailMiddle } from "@xiyun/utils";
+
+export default {
+    filters: { hideMailMiddle },
+    data() {
+        return {
+            mail: '1234323434@163.com',
+        }
+    },
+}
+</script>
+```
--- packages/utils/src/libraries/filters.ts
+++ packages/utils/src/libraries/filters.ts
@@ -14,7 +14,17 @@ const hidePhoneMiddle = function(val) {
     }
     return val.replace(/(\d{3})\d{4}(\d*)/,'$1****$2')
 }
+// 隐藏 邮箱 中间数字 保留首位2位 结尾3位
+const hideMailMiddle = function(val) {
+    if(val.length<6){
+        return;
+    }
+    let temp = val.toString().split('@');
+    return `${temp[0].replace(/(\d{2})\d{4}(\d*)/,'$1****$2')}@${temp[1]}`
+}
+
 export {
     hideIDMiddle,
     hidePhoneMiddle,
+    hideMailMiddle,
 }
