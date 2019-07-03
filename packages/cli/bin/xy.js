49c1dd3797bafccd8be3dd91de334870536cf1dc
zhaoliang <kybetter@163.com>
2019-07-03 16:19:16 +0800

    feat: 完成装载外部插件的逻辑

--- packages/cli/bin/xy.js
+++ packages/cli/bin/xy.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
+const assert = require('assert');
 const yParser = require('yargs-parser');
+const fs = require('fs');
 const Service = require('../lib/Service').default;
+const path = require('path');
+const userHome = require('user-home');
+
 const args = yParser(process.argv.slice(2));
 
 // Plugin List
@@ -9,7 +14,22 @@ const Block = require('@xiyun/xy-plugin-block').default;
 const Create = require('@xiyun/xy-plugin-create').default;
 const Add = require('@xiyun/xy-plugin-add').default;
 
-const service = new Service(args._[0], args, {
-  plugins: [Block, Create, Add],
+const pluginList = [Block, Create, Add];
+
+module.paths.unshift(path.resolve(userHome, '.xy', 'plugins', 'node_modules'));
+
+const xyPluginPkg = path.resolve(userHome, '.xy', 'plugins', 'package.json');
+
+assert(fs.existsSync(xyPluginPkg), 'No plugin installed');
+
+const dependencies = require(xyPluginPkg).dependencies;
+
+Object.keys(dependencies).forEach(item => {
+  pluginList.push(require(item).default);
 });
+
+const service = new Service(args._[0], args, {
+  plugins: pluginList,
+});
+
 service.run();
