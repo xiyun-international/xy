e8e0e7e9ace151787f72dd02b3188f9f5b952f59
yuyue <yuyue@xiyun.com.cn>
2019-07-11 15:12:29 +0800

    fix: xy-plugin-test未完成

--- packages/xy-plugin-test/src/jest.ts
+++ packages/xy-plugin-test/src/jest.ts
@@ -1,40 +1,31 @@
-const jest = require('jest');
+const fs = require('fs');
+const cwd = process.cwd();
+const path = require('path');
 
 const config = {
   transform: {
     '^.+\\.js$': require.resolve('babel-jest'),
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
     '\\.(vue)$': require.resolve('vue-jest'),
   },
+  // serializer for snapshots
+  snapshotSerializers: ['jest-serializer-vue'],
   transformIgnorePatterns: ['node_modules'],
   testMatch: ['**/?*.(spec|test|e2e).(j|t)s?(x)'],
   moduleFileExtensions: ['js', 'jsx', 'vue', 'json'],
   testPathIgnorePatterns: ['/node_modules/'],
 };
-async function run(cmd) {
-  new Promise((resolve, reject) => {
-    jest
-      .runCLI(
-        {
-          config: JSON.stringify(config),
-          ...cmd,
-          colors: true,
-          runInBand: true,
-        },
-        ['./src'],
-      )
-      .then(result => {
-        const results = result.results;
 
-        if (results.success) {
-          resolve();
-        } else {
-          reject(new Error('Jest failed'));
-        }
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  });
+async function run(cmd) {
+  const jestArgs = [
+    '--env',
+    'node',
+    '--runInBand',
+    '-all',
+    JSON.stringify(config),
+  ];
+  require('jest').run(jestArgs);
 }
 
 export default run;
