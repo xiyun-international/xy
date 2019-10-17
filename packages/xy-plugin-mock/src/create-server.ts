09e2c614fd72965de9f9f7cc6561cd70af196479
yuyue <yuyue@xiyun.com.cn>
2019-10-17 15:08:45 +0800

    fix: 解决mock cors 跨域问题

--- packages/xy-plugin-mock/src/create-server.ts
+++ packages/xy-plugin-mock/src/create-server.ts
@@ -55,7 +55,24 @@ export default async function(opts: Options) {
   }
   new Promise((resolve, reject) => {
     const app = express();
+    app.use('*', function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*'); //这个表示任意域名都可以访问，这样写不能携带cookie了。
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild',
+      );
+      res.header(
+        'Access-Control-Allow-Methods',
+        'PUT, POST, GET, DELETE, OPTIONS',
+      ); //设置方法
+      if (req.method == 'OPTIONS') {
+        res.send(200); // 意思是，在正常的请求之前，会发送一个验证，是否可以请求。
+      } else {
+        next();
+      }
+    });
     app.use(function XY_MOCK(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
       const match = mockData && matchMock(req, mockData);
       if (match) {
         signale.info(`mock matched: [${match.method}] ${match.path}`);
@@ -64,6 +81,7 @@ export default async function(opts: Options) {
         return next();
       }
     });
+
     app.use((req, res, next) => {
       if (req.path === '/') {
         res.end(HOME_PAGE);
