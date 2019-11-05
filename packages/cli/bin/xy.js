c987aadeb2318946454d48fdd663350da56ca8dc
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2019-11-05 15:24:50 +0800

    style: code format

--- packages/cli/bin/xy.js
+++ packages/cli/bin/xy.js
@@ -6,6 +6,7 @@ const args = yParser(process.argv.slice(2));
 const pluginList = require('../lib/getPlugins').default;
 const updateNotifier = require('update-notifier');
 const pkg = require('../package.json');
+
 updateNotifier({ pkg }).notify({ defer: true });
 
 const service = new Service(args._[0], args, {
