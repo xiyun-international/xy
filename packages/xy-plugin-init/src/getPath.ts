0a9cbc22bf0e1e557d8a7855b210d2a0911ca271
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2019-11-15 16:25:58 +0800

    fix: 修复init失效问题

--- packages/xy-plugin-init/src/getPath.ts
+++ packages/xy-plugin-init/src/getPath.ts
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 
 export const getPath = filename => {
   const cwd = process.cwd();
-  const sourceFile = resolve(__dirname, 'dotfiles', filename);
+  const sourceFile = resolve('../template', filename);
   const target = resolve(cwd, filename);
   return [sourceFile, target];
 };
