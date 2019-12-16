9d00cd80190d0779807e3ef411e81eeb74214e69
huqiangqiang <huqiangqiang@xiyun.com.cn>
2019-12-16 19:14:55 +0800

    feat: 新增公告组件

---
+++ packages/element-ui/packages/notice/index.js
@@ -0,0 +1,7 @@
+import XyNotice from './xy-notice.vue';
+
+XyNotice.install = function(Vue) {
+  Vue.component(XyNotice.name, XyNotice);
+};
+
+export default XyNotice;
---
+++ packages/element-ui/packages/notice/xy-notice.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="noticeTitle" v-show="isNoticeShow">
+    {{ title }}
+    <span class="noticeClose" @click="closeNotice" v-show="isClose">×</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XyNotice',
+  props: {
+      title: {
+          type: String,
+          default() {
+              return '';
+          },
+      },
+
+    isNoticeShow: {
+        type: Boolean,
+        default() {
+            return false;
+        },
+    },
+
+    isClose: {
+        type: Boolean,
+        default() {
+            return false;
+        },
+    },
+  },
+  methods: {
+    /**
+     *  关闭公告栏
+     */
+    closeNotice() {
+      this.isNoticeShow = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.noticeTitle {
+  width: 100%;
+  color: #f68b7a;
+  background: #fffecc;
+  text-align: center;
+  height: 40px;
+  line-height: 40px;
+}
+
+.noticeClose {
+  font-size: 20px;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 23px;
+}
+</style>
