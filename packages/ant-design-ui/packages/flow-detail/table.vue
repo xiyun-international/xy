03e3b8f1407d821343a24f359aa22a8780a08345
yuyue <yuyue@xiyun.com.cn>
2020-03-05 14:01:57 +0800

    fix: flow-detail 中图片字段统一调整

--- packages/ant-design-ui/packages/flow-detail/table.vue
+++ packages/ant-design-ui/packages/flow-detail/table.vue
@@ -33,7 +33,7 @@
               <template v-for="(img, imgIndex) in td.value">
                 <div :key="imgIndex" class="d-LB img-block">
                   <div>
-                  <img :src="img.src" :data-tittle="img.name" />
+                  <img :src="img.src || img.small" :data-view="img.url" :data-tittle="img.name" />
                   </div>
                   <a v-if="td.components === 'download'"
                     :href="img.downloadUrl" style="display: block;"
